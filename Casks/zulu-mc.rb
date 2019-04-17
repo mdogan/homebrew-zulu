602869aaa61ec0c04b5f3db959ba56e6de58304d
Mehmet Dogan <mehmet@dogan.io>
2019-04-17 11:47:10 +0300

    Fix Zulu Mission Control

--- Casks/zulu-mc.rb
+++ Casks/zulu-mc.rb
@@ -2,12 +2,12 @@ cask 'zulu-mc' do
     version '7.0.0-EA'
     sha256 :no_check
 
-    url "http://static.azul.com/zmc/bin/zmc#{version}-macosx_x64.tar.gz",
+    url "https://static.azul.com/zmc/bin/zmc#{version}-macosx_x64.tar.gz",
         referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
     name 'Zulu® Mission Control'
     homepage 'https://www.azul.com/products/zulu-mission-control'
 
     depends_on macos: '>= :sierra'
 
-    app 'Zulu Mission Control.app'
+    app "zmc#{version}-macosx_x64/Zulu Mission Control.app"
   end
