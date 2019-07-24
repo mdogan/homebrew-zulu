e2afc6b2de5f3ed7c5a2f5d483ff9ef0e9814f37
Mehmet Dogan <mehmet@dogan.io>
2019-07-24 10:16:51 +0300

    Fix mission control url

--- Casks/zulu-mc.rb
+++ Casks/zulu-mc.rb
@@ -2,7 +2,7 @@ cask 'zulu-mc' do
     version '7.0.0.49-ca'
     sha256 :no_check
 
-    url "https://static.azul.com/zmc/bin/zmc#{version}-macosx_x64.tar.gz",
+    url "https://static.azul.com/zmc/bin/zmc#{version}-macos_x64.tar.gz",
         referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
     name 'Zulu® Mission Control'
     homepage 'https://www.azul.com/products/zulu-mission-control'
