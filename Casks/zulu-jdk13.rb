fff6d18aa6c794aa41b4d992d9ee22ea2fb1b39f
Mehmet Dogan <mehmet@dogan.io>
2019-09-18 10:19:46 +0300

    Fix JDK 13 version

--- Casks/zulu-jdk13.rb
+++ Casks/zulu-jdk13.rb
@@ -1,5 +1,5 @@
 cask 'zulu-jdk13' do
-    version '13.29.9,13'
+    version '13.27.9,13'
     sha256 'f9972f622c489d6cfab2593f67caf4e18d27ca0a4c8a5ef5d70176d9ea6520ff'
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_x64.dmg",
