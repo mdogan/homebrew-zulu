a193f86864b254921b56bb71895ddeca2fd4f089
Mehmet Dogan <mehmet@dogan.io>
2020-03-20 15:19:07 +0300

    Add JDK 14 and update JDK 8 and MC casks

--- Casks/zulu-jdk14.rb
+++ Casks/zulu-jdk14.rb
@@ -1,10 +1,10 @@
-cask 'zulu-jdk8' do
-    version '8.0.242,8.44.0.11'
-    sha256 '7bfaa4899f41468a216e15438cf0b9b0681dde81520dd6a5f1f01a3b20914ef8'
+cask 'zulu-jdk14' do
+    version '14.27.1,14'
+    sha256 '275b0e4953318fa4c03f178dca2860b0491178cfaa9e464b351e774e56a3dd01'
 
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-ca-jdk#{version.before_comma}-macosx_x64.dmg",
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_x64.dmg",
         referer: 'https://www.azul.com/downloads/zulu-community/'
-    name 'Azul Zulu® JDK 8'
+    name 'Azul Zulu® JDK 14'
     homepage 'https://www.azul.com/downloads/zulu-community/'
 
     depends_on macos: '>= :high_sierra'
