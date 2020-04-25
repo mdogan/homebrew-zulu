629d460d3efcbd979abfca1025c58b77f1413f44
Andrey Korzinev <goodfella@yandex-team.ru>
2020-04-25 12:12:17 +0300

    Update zulu-jdk11.rb

--- Casks/zulu-jdk11.rb
+++ Casks/zulu-jdk11.rb
@@ -1,6 +1,6 @@
 cask 'zulu-jdk11' do
-    version '11.37.17,11.0.6'
-    sha256 'bcc8ec14b084140941092e16c8d5183964f3c512a354535770897b0cf542730d'
+    version '11.39.15,11.0.7'
+    sha256 '8322cb6498076f171ab5efa4101fe8207d9aa6517d1d743bbe4d753b67abc8cb'
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_x64.dmg",
         referer: 'https://www.azul.com/downloads/zulu-community/'
