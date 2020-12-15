c71a68f0527fe6584675ef2c2e31a08816873a17
RORY KELLY <rorykelly@rorys-mbp.mynet>
2020-12-15 12:00:15 +0000

    fixing url

--- Casks/zulu-jdk11-aarch64.rb
+++ Casks/zulu-jdk11-aarch64.rb
@@ -1,8 +1,8 @@
 cask 'zulu-jdk11-aarch64' do
     version '11.43.1015,11.0.9.1'
     sha256 'e6e55cb09b535a3d195a9b1719613842b6af01e6e7536c98fddb1cd28fd44f78'
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_aarch64.dmg",
+    
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macos_aarch64.dmg",
         referer: 'https://www.azul.com/downloads/zulu-community/'
     name 'Azul Zulu® JDK 11'
     homepage 'https://www.azul.com/downloads/zulu-community/'
