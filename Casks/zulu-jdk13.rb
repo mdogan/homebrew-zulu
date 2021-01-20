268be4804492904712de7b08f5b73dab394b5351
Mehmet Dogan <mehmet@dogan.io>
2021-01-20 11:22:44 +0300

    Update JDK 13

--- Casks/zulu-jdk13.rb
+++ Casks/zulu-jdk13.rb
@@ -1,6 +1,6 @@
 cask 'zulu-jdk13' do
-    version '13.35.17,13.0.5'
-    sha256 'ce0b48f881bcb1d347b1e98a731aea34b843c2e5c9a38a93b14b364a0b10d36c'
+    version '13.35.51,13.0.5.1'
+    sha256 '6dd2115f2124e84b4c6eea160c57a64fc7be3515c659a91656ed2369d0f744bc'
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_x64.dmg",
         referer: 'https://www.azul.com/downloads/zulu-community/'
