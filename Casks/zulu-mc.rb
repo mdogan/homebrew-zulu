f367e36b90a4684f3d22fc5b0f5f856ca027eb75
Mehmet Dogan <mehmet@dogan.io>
2021-09-15 22:53:23 +0300

    Fix MC download URL

--- Casks/zulu-mc.rb
+++ Casks/zulu-mc.rb
@@ -2,7 +2,7 @@ cask 'zulu-mc' do
     version '8.1.0.42-ca'
     sha256 :no_check
 
-    url "https://static.azul.com/zmc/bin/zmc#{version}-macos_x64.tar.gz",
+    url "https://cdn.azul.com/zmc/bin/zmc#{version}-macos_x64.tar.gz",
         referer: 'https://www.azul.com/products/zulu-mission-control/'
     name 'Zulu® Mission Control'
     homepage 'https://www.azul.com/products/zulu-mission-control/'
