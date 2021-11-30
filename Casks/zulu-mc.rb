4e2f83b931c51655e4941b45bdf48aa39dc6dc83
Mehmet Dogan <mehmet@dogan.io>
2021-11-30 11:04:26 +0300

    Update Mission Control, add arm64 build

--- Casks/zulu-mc.rb
+++ Casks/zulu-mc.rb
@@ -1,14 +1,26 @@
 cask 'zulu-mc' do
-    version '8.1.0.42-ca'
+
+  if Hardware::CPU.intel?
+    version '8.1.1.51-ca'
     sha256 :no_check
 
     url "https://cdn.azul.com/zmc/bin/zmc#{version}-macos_x64.tar.gz",
         referer: 'https://www.azul.com/products/zulu-mission-control/'
-    name 'Zulu® Mission Control'
-    homepage 'https://www.azul.com/products/zulu-mission-control/'
 
     depends_on macos: '>= :high_sierra'
+  else
+    version '8.1.1.57-ca'
+    sha256 :no_check
 
-    app "zmc#{version}-macos_x64/Azul Mission Control.app"
+    url "https://cdn.azul.com/zmc/bin/zmc#{version}-macos_aarch64.tar.gz",
+        referer: 'https://www.azul.com/products/zulu-mission-control/'
 
+    depends_on macos: '>= :big_sur'
   end
+
+  name 'Zulu® Mission Control'
+  homepage 'https://www.azul.com/products/zulu-mission-control/'
+
+  app "zmc#{version}-macos_x64/Azul Mission Control.app"
+
+end
