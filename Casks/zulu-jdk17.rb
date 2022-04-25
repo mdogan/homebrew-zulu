52719994f6cdf1f80f42eadd787fba01c42fa208
Saqib Saleem <64003679+ssaleem-ee@users.noreply.github.com>
2022-04-25 08:11:49 +0100

    Bump JDK 17 version to 17.0.3

--- Casks/zulu-jdk17.rb
+++ Casks/zulu-jdk17.rb
@@ -1,18 +1,18 @@
 cask 'zulu-jdk17' do
 
   if Hardware::CPU.intel?
-    version '17.32.13,17.0.2'
-    sha256 '73d9fd535a690c1fdcc9099edbb7f9e9b8897e30573c0be95360e6896804c203'
+    version '17.34.19,17.0.3'
+    sha256 '275a6bd5719bfc740f2ec729f42c0e032e8887351f651a00ff7f254e7ab9c2ed'
 
-    url 'https://cdn.azul.com/zulu/bin/zulu17.32.13-ca-jdk17.0.2-macosx_x64.dmg',
+    url 'https://cdn.azul.com/zulu/bin/zulu17.34.19-ca-jdk17.0.3-macosx_x64.dmg',
         referer: 'https://www.azul.com/downloads/zulu-community/'
 
     depends_on macos: '>= :mojave'
   else
-    version '17.32.13,17.0.2'
-    sha256 '74d8e059865fdb8cfbc202bbd93a05c5a67a2d12947b09e30d637dd9b7e4011f'
+    version '17.34.19,17.0.3'
+    sha256 '100c4cd1325a3bf8e5731ff4ba625d04d0e460198b548626d711e48b9ad6fba9'
 
-    url 'https://cdn.azul.com/zulu/bin/zulu17.32.13-ca-jdk17.0.2-macosx_aarch64.dmg',
+    url 'https://cdn.azul.com/zulu/bin/zulu17.34.19-ca-jdk17.0.3-macosx_aarch64.dmg',
         referer: 'https://www.azul.com/downloads/zulu-community/'
 
     depends_on macos: '>= :big_sur'
