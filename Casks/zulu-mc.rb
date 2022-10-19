a29ce540bbe471f8ee27dddb43e84249eb4cfd50
Mehmet Dogan <mehmet@dogan.io>
2022-10-19 12:50:32 +0300

    Update Mission Control arm64 version

--- Casks/zulu-mc.rb
+++ Casks/zulu-mc.rb
@@ -15,7 +15,7 @@ cask 'zulu-mc' do
     app "zmc#{version}-macos_x64/Azul Mission Control.app"
 
   else
-    version '8.1.1.57-ca'
+    version '8.2.1.67-ca'
     sha256 :no_check
 
     url "https://cdn.azul.com/zmc/bin/zmc#{version}-macos_aarch64.tar.gz",
