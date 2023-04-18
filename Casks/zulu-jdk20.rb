34992dcee4671c84a2457c4a347d3c9a99a98c5d
mdogan <mdogan@users.noreply.github.com>
2023-04-18 21:10:51 +0000

    Upgrade JDK Casks to the latest versions

--- Casks/zulu-jdk20.rb
+++ Casks/zulu-jdk20.rb
@@ -1,18 +1,18 @@
 cask 'zulu-jdk20' do
 
   if Hardware::CPU.intel?
-    version '20.28.85,20.0.0'
-    sha256 'b7978dc81acc7864f7c90034104e3a8d9e4e2d150ee288f175dfdab77438d992'
+    version '20.30.11,20.0.1'
+    sha256 '16322d4efd15adf1787a0ef0ddef5dc00382db72c57ad9bc6c2940debce7971b'
 
-    url 'https://cdn.azul.com/zulu/bin/zulu20.28.85-ca-jdk20.0.0-macosx_x64.dmg',
+    url 'https://cdn.azul.com/zulu/bin/zulu20.30.11-ca-jdk20.0.1-macosx_x64.dmg',
         referer: 'https://www.azul.com/downloads/zulu-community/'
 
     depends_on macos: '>= :mojave'
   else
-    version '20.28.85,20.0.0'
-    sha256 '0529683697ef3e0f0c26aa63dbbb9848b0f359b14c40ed2720adf3459ca539a1'
+    version '20.30.11,20.0.1'
+    sha256 'f712f5b7b042b0f3b15a3905f1f08e1a5580521329f346d18612efed1b3375eb'
 
-    url 'https://cdn.azul.com/zulu/bin/zulu20.28.85-ca-jdk20.0.0-macosx_aarch64.dmg',
+    url 'https://cdn.azul.com/zulu/bin/zulu20.30.11-ca-jdk20.0.1-macosx_aarch64.dmg',
         referer: 'https://www.azul.com/downloads/zulu-community/'
 
     depends_on macos: '>= :big_sur'
