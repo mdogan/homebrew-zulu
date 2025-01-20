2107fca84f23caf53fbae8c1e2c87c66ad5ac8d2
Goooler <wangzongler@gmail.com>
2025-01-20 15:45:08 +0800

    Update pinned casks and workflows

--- Casks/zulu-jdk20.rb
+++ Casks/zulu-jdk20.rb
@@ -1,6 +1,6 @@
 cask 'zulu-jdk20' do
 
-  if Hardware::CPU.intel?
+  on_intel do
     version '20.32.11,20.0.2'
     sha256 '6f02981801832230edd0a0091b8de10873d3271cd7cc4d7a9ed439b046fa4b1f'
 
@@ -8,7 +8,8 @@ cask 'zulu-jdk20' do
         referer: 'https://www.azul.com/downloads/zulu-community/'
 
     depends_on macos: '>= :mojave'
-  else
+  end
+  on_arm do
     version '20.32.11,20.0.2'
     sha256 'c5c196a0a25eca25a22edee325ff8ee2cd96aa9bfdee0ca3554dfbfd31dba8df'
 
