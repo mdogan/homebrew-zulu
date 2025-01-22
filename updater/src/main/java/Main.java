f583cc386fce9d59fecad0cb6e45d364e931c929
Goooler <wangzongler@gmail.com>
2025-01-22 19:03:45 +0800

    No need to trigger actions after .github/workflows/jdk{VERSION}.yml changed

--- updater/src/main/java/Main.java
+++ updater/src/main/java/Main.java
@@ -226,14 +226,12 @@ on:
       - master
     paths:
       - '.github/workflows/reusable-cask-checks.yml'
-      - '.github/workflows/jdk{VERSION}.yml'
       - 'Casks/zulu-jdk{VERSION}.rb'
   pull_request:
     branches:
       - master
     paths:
       - '.github/workflows/reusable-cask-checks.yml'
-      - '.github/workflows/jdk{VERSION}.yml'
       - 'Casks/zulu-jdk{VERSION}.rb'
 
 jobs:
