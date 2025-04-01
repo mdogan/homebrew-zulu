6d1c384f8ca98daf334dacd278c28f1b3684ed17
Goooler <wangzongler@gmail.com>
2025-04-01 11:38:35 +0800

    Update Mission Control to 9.1.0.25
    https://www.azul.com/products/components/azul-mission-control/

--- Casks/zulu-mc.rb
+++ Casks/zulu-mc.rb
@@ -1,25 +1,25 @@
 cask 'zulu-mc' do
 
   name 'Zulu® Mission Control'
-  homepage 'https://www.azul.com/products/zulu-mission-control/'
+  homepage 'https://www.azul.com/products/components/azul-mission-control/'
 
   on_intel do
-    version '8.3.1.81-ca'
+    version '9.1.0.25-ca'
     sha256 :no_check
 
     url "https://cdn.azul.com/zmc/bin/zmc#{version}-macos_x64.tar.gz",
-        referer: 'https://www.azul.com/products/zulu-mission-control/'
+        referer: 'https://www.azul.com/products/components/azul-mission-control/'
 
     depends_on macos: '>= :mojave'
 
     app "zmc#{version}-macos_x64/Azul Mission Control.app"
   end
   on_arm do
-    version '8.3.1.81-ca'
+    version '9.1.0.25-ca'
     sha256 :no_check
 
     url "https://cdn.azul.com/zmc/bin/zmc#{version}-macos_aarch64.tar.gz",
-        referer: 'https://www.azul.com/products/zulu-mission-control/'
+        referer: 'https://www.azul.com/products/components/azul-mission-control/'
 
     depends_on macos: '>= :big_sur'
 
