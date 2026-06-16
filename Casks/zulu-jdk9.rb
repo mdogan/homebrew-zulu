05496f9b792e2ef9b13dc496f5540d854eaa6a2b
mdogan <1201712+mdogan@users.noreply.github.com>
2026-06-16 20:47:20 +0000

    Upgrade JDK Casks to the latest versions

--- Casks/zulu-jdk9.rb
+++ Casks/zulu-jdk9.rb
@@ -3,7 +3,7 @@ cask 'zulu-jdk9' do
     version '9.0.7.1,9.0.7'
     sha256 'ddc362256ce2a06a37993cbe1ae2c166191444c23875d10c38bef910d5b5e6ac'
 
-    url 'https://cdn.azul.com/zulu/bin/zulu9.0.7.1-ca-jdk9.0.7-macosx_x64.dmg',
+    url 'https://cdn.azul.com/zulu/bin/zulu9.0.7.1-jdk9.0.7-macosx_x64.dmg',
         referer: 'https://www.azul.com/downloads/?os=macos&package=jdk#zulu'
 
 
